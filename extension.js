0d3fa15eda20eeb1678e27108f086e173901000b
caleb cauthon <caleb.cauthon@pricefx.com>
2023-04-29 00:37:21 -0500

    Separate out the prompts

--- extension.js
+++ extension.js
@@ -6,9 +6,21 @@ const trySafely = require('./lib/trySafely');
 function activate(context) {
 	let processSelectedTextCommand = vscode.commands.registerCommand('magic-code.processSelectedText', () => {
 		trySafely.trySafely(() => {
+			const template = {
+				setup: "Add snarky in-line comments to the following javascript code.",
+				prompt: (data) => `
+				-------------
+				
+				${data.selectedText}
+				
+				-------------
+				End of File
+				
+				Include the code, which is between the "-------", in your response.`
+			};
 			const config = vscode.workspace.getConfiguration('magic-code');
 			localAPI.setApiKey(config.get("openai_api_key"));
-			goCommand.init(vscode, localAPI.callExternalAPI);
+			goCommand.init(vscode, (text) => localAPI.callExternalAPI(template, text));
 			goCommand.processSelectedText();
 		});
 	});
